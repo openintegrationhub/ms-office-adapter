486e8c250baa5207698e4377ba238aa982e32dea
jkopen <jknoop@wice.de>
2020-09-01 11:59:16 +0200

    Adding ferryman and replacing outdated microsoft api with microsoft graph api

---
+++ test/base.test.js
@@ -0,0 +1,37 @@
+const { expect } = require('chai');
+const nock = require('nock');
+
+const ApiClient = require('../lib/apiClient');
+
+let apiClient;
+
+describe('apiClient for microsoft', () => {
+  before(async () => {
+    apiClient = new ApiClient({
+      accessToken: 'SomeToken',
+    });
+
+    const getCallSuccess = nock('http://localhost:3099')
+      .get('/v1.0/me/contacts')
+      .reply(200, {hello: 'test'});
+
+    const postCallSuccess = nock('http://localhost:3099')
+      .post('/v1.0/me/contacts')
+      .reply(200, {hello: 'test2'});
+  });
+
+  it('Do a get call to microsoft correctly', async () => {
+    const result = await apiClient.get('/me/contacts', [{test: 1}]);
+    expect(result).to.be.an('object');
+    expect(result).to.have.key('hello');
+    expect(result.hello).to.equal('test');
+  });
+
+  it('Do a post call to microsoft correctly', async () => {
+    const result = await apiClient.post('/me/contacts', [{test: 1}]);
+    expect(result).to.be.an('object');
+    expect(result).to.have.key('hello');
+    expect(result.hello).to.equal('test2');
+  });
+
+});
